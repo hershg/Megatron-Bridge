f454fc3d33793dbb362efa972d20782d72860d69
yaoyu-33 <yaoyu.094@gmail.com>
2026-08-25 22:43:43 -0700

    fix(recipe): bind Qwen HybridEP to physical nodes
    Signed-off-by: yaoyu-33 <yaoyu.094@gmail.com>

--- tests/unit_tests/recipes/test_qwen_recipes.py
+++ tests/unit_tests/recipes/test_qwen_recipes.py
@@ -878,7 +878,7 @@ def test_qwen3_235b_blackwell_main_recipes_match_measured_perf_settings(
     assert {k: v for k, v in main_cfg.env_vars.items() if k != hybridep_domain_key} == {
         k: v for k, v in perf_cfg.env_vars.items() if k != hybridep_domain_key
     }
-    assert hybridep_domain_key not in main_cfg.env_vars
+    assert main_cfg.env_vars[hybridep_domain_key] == 4
     assert perf_cfg.env_vars[hybridep_domain_key] == 32
     assert main_cfg.logger.tensorboard_dir == perf_cfg.logger.tensorboard_dir is None
     assert main_cfg.train.global_batch_size == perf_cfg.train.global_batch_size
